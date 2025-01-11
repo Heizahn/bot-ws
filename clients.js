97c8f56f212127b7ceba46007989c2476e40f8f5
Heizahn <humbertorbw@gmail.com>
2025-01-11 10:43:42 -0400

    ready 1.2

--- clients.js
+++ clients.js
@@ -1,6 +1,6 @@
 module.exports = {
 	clients: async () => {
-		const res = await fetch('http://localhost:3000/clients-for-bot');
+		const res = await fetch('http://172.17.0.126:3000/clients-for-bot');
 		const data = await res.json();
 		return data;
 	},
